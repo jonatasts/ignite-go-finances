db928a1d8e8df267c8df187ebf7add1b92f41c91
Jonatas Santos <jonatas.santos@produsti.com.br>
2023-01-12 08:29:25 -0300

    ✨ Create card list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,8 @@
-import React from "React";
+import React from "react";
 import { StatusBar } from "react-native";
 import { useTheme } from "styled-components";
+
+import { HighlightCard } from "../../components/HighlightCard";
 
 import {
   Container,
@@ -12,6 +14,7 @@ import {
   UserGreetting,
   UserName,
   Icon,
+  HighlightCards,
 } from "./styles";
 
 export const Dashboard = () => {
@@ -38,6 +41,27 @@ export const Dashboard = () => {
           <Icon name={"power"} />
         </UserWrapper>
       </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entrada"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 };
