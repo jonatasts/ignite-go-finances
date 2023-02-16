e5c7dc467bf38cd995817e62855a84538bdf4843
jonatasts <jonatassantos96@hotmail.com>
2023-02-16 14:57:06 -0300

    ✨ Create category select button component

---
+++ src/components/Forms/CategorySelect/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { Container, Category, Icon } from "./styles";
+
+interface CategorySelectProps extends RectButtonProps {
+  category: string;
+}
+
+export function CategorySelectButton({
+  activeOpacity,
+  category,
+  ...rest
+}: CategorySelectProps) {
+  return (
+    <Container {...rest}>
+      <Category>{category}</Category>
+      <Icon name="chevron-down" />
+    </Container>
+  );
+}
