2c55f53132bed5480c8f473fb98d78919a115caa
jonatasts <jonatassantos96@hotmail.com>
2023-02-16 15:26:03 -0300

    ✨ Create category select screen

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
+} from "./styles";
+
+import { Button } from "../../components/Forms/Button";
+
+import { categories } from "../../utils/categories";
+
+interface Category {
+  key: string;
+  name: string;
+}
+
+interface CategorySelectProps {
+  category: Category;
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
+}
+
+export const CategorySelect = ({
+  category,
+  setCategory,
+  closeSelectCategory,
+}: CategorySelectProps) => {
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList
+        data={categories}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category
+            isActive={category.key === item.key}
+            onPress={() => setCategory(item)}
+          >
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+        style={{ flex: 1, width: "100%" }}
+      />
+
+      <Footer>
+        <Button onPress={closeSelectCategory} title="Selecionar" />
+      </Footer>
+    </Container>
+  );
+};
---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+
+interface CategoryProps {
+  isActive: boolean;
+}
+
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.background};
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: flex-end;
+  height: ${RFValue(113)}px;
+  padding-bottom: 19px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Category = styled.TouchableOpacity<CategoryProps>`
+  align-items: center;
+  background-color: ${({ theme, isActive }) =>
+    isActive ? theme.colors.secondary_light : theme.colors.background};
+  flex-direction: row;
+  padding: ${RFValue(15)}px;
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  margin-right: ${RFValue(16)}px;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`;
+
+export const Separator = styled.View`
+  background-color: ${({ theme }) => theme.colors.text};
+  height: 1px;
+`;
+
+export const Footer = styled.View`
+  padding: 24px;
+`;
