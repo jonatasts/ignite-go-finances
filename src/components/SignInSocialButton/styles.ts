b56915a8f09d3c6011b874f6307af321334f90b2
jonatasts <jonatassantos96@hotmail.com>
2023-03-02 15:39:06 -0300

    ✨ Create SignInSocialButton component

---
+++ src/components/SignInSocialButton/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const Button = styled(TouchableOpacity).attrs({
+  activeOpacity: 0.8,
+})`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 5px;
+  flex-direction: row;
+  height: ${RFValue(56)}px;
+  margin-bottom: 16px;
+`;
+
+export const ImageContainer = styled.View`
+  align-items: center;
+  border-color: ${({ theme }) => theme.colors.background};
+  border-right-width: 1px;
+  justify-content: center;
+  padding: ${RFValue(16)}px;
+`;
+
+export const Text = styled.Text`
+  flex: 1;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(14)}px;
+  text-align: center;
+`;
